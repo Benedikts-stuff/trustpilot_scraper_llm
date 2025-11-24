8ede75966e4d7573f3233163219a78672012e93b
Benedikt <benedikt_engel@gmx.de>
2025-11-24 21:12:57 +0100

    Dash

--- pipeline/config.py
+++ pipeline/config.py
@@ -5,7 +5,7 @@ import os
 GROQ_API_KEY =os.getenv("GROQ_API_KEY")
 
 # Sentiment-Wortlisten (aus word_list_sentiment.py)
-SENTIWS_POS_PATH = "SentiWS_v2.0_Positive.txt"ß
+SENTIWS_POS_PATH = "SentiWS_v2.0_Positive.txt"
 SENTIWS_NEG_PATH = "SentiWS_v2.0_Negative.txt"
 STOPWORDS_PATH = "german_stopwords_full.txt"
 
